676f2eda5e31d1c32b5ee89866a8728ef56e0b72
Devyani Kolhe <devyanikolhe02@gmail.com>
2025-09-10 12:33:44 +0530

    Initial commit - existing Django project

---
+++ innovapp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class InnovappConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "innovapp"
---
+++ innovapp/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from . import views as v
+
+urlpatterns = [
+    path("", v.home),
+    path('aboutus',v.aboutus),
+]
