3ee38b03f438325174bb84e9ffdb6a32f65410bd
Jyoti Dattatraya Nanaware <jyonanaware2002@gmail.com>
2025-09-15 13:47:39 +0530

    service toggle view

--- innovapp/static/js/script.js
+++ innovapp/static/js/script.js
@@ -1,22 +1,17 @@
-// FAQ toggle
-document.addEventListener("DOMContentLoaded", () => {
-  const faqButtons = document.querySelectorAll(".faq-q");
-  faqButtons.forEach(btn => {
-    btn.addEventListener("click", () => {
-      const answer = btn.nextElementSibling;
-      if(answer.style.display === "block"){
-        answer.style.display = "none";
-      } else {
-        answer.style.display = "block";
-      }
-    });
-  });
+// document.addEventListener("DOMContentLoaded", () => {
 
-  // Learn more alert
-  const learnBtns = document.querySelectorAll(".learn-more");
-  learnBtns.forEach(btn => {
-    btn.addEventListener("click", () => {
-      alert(`You clicked on: ${btn.dataset.service}`);
-    });
-  });
-});
+// });
+
+//   function toggleMenu() {
+//     document.querySelector("nav ul").classList.toggle("show");
+//   }
+  
+//   // Dropdown open/close on mobile
+//   document.querySelectorAll(".dropdown > a").forEach(link => {
+//     link.addEventListener("click", e => {
+//       if (window.innerWidth <= 768) {
+//         e.preventDefault(); // prevent redirect
+//         link.parentElement.classList.toggle("open");
+//       }
+//     });
+//   });
